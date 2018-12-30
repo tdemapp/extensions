d44cd10cb07cd0401d1dbbc10b3832d3fbfe8797
N U R O ™ <4991309+NuroDev@users.noreply.github.com>
2018-12-30 22:22:04 +0000

    :tada: Added initial template extension

---
+++ template.js
@@ -0,0 +1,16 @@
+{
+	name: "myExtension",
+	author: "myExtensionAuthor",
+	description: "This is myExtension!",
+	version: "1.1.1",
+	website: "https://gist.github.com/",
+	isEnabled: false,
+	dependencies: ["myImportantExtension"],
+	conflicts: ["veryBadExtension"],
+	create: () => {
+		console.log("myExtension created!");
+	},
+	destroy: () => {
+		console.log("myExtension destroyed!");
+	},
+}
