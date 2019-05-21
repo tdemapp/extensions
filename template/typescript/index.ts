78c1500634e2b37841773dcfe2adbb9b74762f83
N U R O ™ <bendixon50@gmail.com>
2019-05-21 11:58:45 +0100

    :sparkles: Added TS template

---
+++ template/typescript/index.ts
@@ -0,0 +1,12 @@
+const create = ():void => {
+	console.log("myExtension created!");
+};
+
+const destroy = ():void => {
+	console.log("myExtension destroyed!");
+};
+
+export {
+  create,
+  destroy,
+}
